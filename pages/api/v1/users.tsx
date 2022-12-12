1d748d8fbdae74cee40503c9bcb7530b16f0fcb2
李齐一 <errgou@gmail.com>
2022-12-12 16:32:24 +0800

    修改用户密码校验

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -4,40 +4,24 @@ import {User} from "../../../src/entity/User";
 import md5 from "md5";
 
 const Users: NextApiHandler = async (req, res) => {
-	const {username, password, passwordConfirmation} = req.body;
-	const errors = {
-		username: [] as string[], password: [] as string[], passwordConfirmation: [] as string[]
-	};
-	if (username.trim() === "") {
-		errors.username.push("不能为空");
-	}
-	if (!/[a-zA-Z0-9]/.test(username.trim())) {
-		errors.username.push("格式不合法");
-	}
-	if (username.trim().length > 42) {
-		errors.username.push("太长");
-	}
-	if (username.trim().length <= 3) {
-		errors.username.push("太短");
-	}
-	if (password === "") {
-		errors.password.push("不能为空");
-	}
-	if (password !== passwordConfirmation) {
-		errors.passwordConfirmation.push("密码不匹配");
-	}
-	const hasErrors = Object.values(errors).find(v => v.length > 0);
+	const {username = "", password, passwordConfirmation} = req.body;
+	if (!AppDataSource.isInitialized) await AppDataSource.initialize();
 	res.setHeader("Content-Type", "application/json; charset=utf-8");
-	if (hasErrors) {
+
+	const userRepository = AppDataSource.getRepository("User");
+	const user = new User();
+	user.username = username;
+	user.username = username;
+	user.passwordDigest = md5(password);
+	user.passwordConfirmation = passwordConfirmation;
+	user.password = password;
+	await user.validate();
+	const [hasError,errors]=user.hasErrors()
+
+	if (hasError) {
 		res.statusCode = 422;
 		res.write(JSON.stringify(errors));
 	} else {
-		const user = new User();
-		user.username = username;
-		if (!AppDataSource.isInitialized) await AppDataSource.initialize();
-		const userRepository = AppDataSource.getRepository("User");
-		user.username = username;
-		user.passwordDigest = md5(password);
 		const u = await userRepository.save(user);
 		res.statusCode = 200;
 		res.write(JSON.stringify(u));
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,7 @@
 import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 import {Post} from "./Post";
 import {Comment} from "./Comment";
+import {AppDataSource} from "../data-source";
 
 @Entity("users")
 export class User {
@@ -10,9 +11,9 @@ export class User {
 	username: string | undefined;
 	@Column("varchar")
 	passwordDigest: string | undefined;
-	@CreateDateColumn({type:"timestamp"})
+	@CreateDateColumn({type: "timestamp"})
 	createdAt: Date | undefined;
-	@UpdateDateColumn({type:"timestamp"})
+	@UpdateDateColumn({type: "timestamp"})
 	updatedAt: Date | undefined;
 
 	@OneToMany(type => Post, post => post.author)
@@ -20,4 +21,51 @@ export class User {
 
 	@OneToMany(type => Comment, comment => comment.user)
 	comments: Comment[] | undefined;
+
+	password: string | undefined;
+	passwordConfirmation: string | undefined;
+
+	errors = {
+		username: [] as string[],
+		password: [] as string[],
+		passwordConfirmation: [] as string[]
+	};
+
+	async validate() {
+
+		// @ts-ignore
+		if (this.username.trim() === "") {
+			this.errors.username.push("不能为空");
+		}
+		// @ts-ignore
+		if (!/[a-zA-Z0-9]/.test(this.username.trim())) {
+			this.errors.username.push("格式不合法");
+		}
+		// @ts-ignore
+		if (this.username.trim().length > 42) {
+			this.errors.username.push("太长");
+		}
+		// @ts-ignore
+		if (this.username.trim().length <= 3) {
+			this.errors.username.push("太短");
+		}
+		if (this.password === "") {
+			this.errors.password.push("不能为空");
+		}
+		console.log(this.password,this.passwordConfirmation);
+		if (this.password !== this.passwordConfirmation) {
+			this.errors.passwordConfirmation.push("密码不匹配");
+		}
+
+		if (!AppDataSource.isInitialized) await AppDataSource.initialize();
+		const userRepository = AppDataSource.getRepository("User");
+		const hasUser = await userRepository.findOne({where: {username: this.username}});
+		if (hasUser) {
+			this.errors.username.push("用户名已存在");
+		}
+	}
+
+	hasErrors() {
+		return [!!Object.values(this.errors).find(v => v.length > 0), this.errors];
+	}
 }
