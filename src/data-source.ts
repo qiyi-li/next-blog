0ff790b4a0960bb3ffceb9019c31bb8ff22b00f6
李齐一 <errgou@gmail.com>
2023-01-13 11:29:26 +0800

    修改数据库

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,16 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import {DataSource} from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "blog",
-    password: "",
-    database: "blog_development",
-    synchronize: false,
-    logging: false,
-    entities: ['dist/entity/*.js'],
-    migrations: ["dist/migration/*.js"],
-    subscribers: ["dist/subscriber/**/*.js"]
-})
+	type: "postgres",
+	host: "localhost",
+	port: 5432,
+	username: "blog",
+	password: "",
+	database: process.env.NODE_ENV === "production" ? "blog_production" : "blog_development",
+	synchronize: false,
+	logging: false,
+	entities: ["dist/entity/*.js"],
+	migrations: ["dist/migration/*.js"],
+	subscribers: ["dist/subscriber/**/*.js"]
+});
