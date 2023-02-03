5b49301309dced82b69eb58bd3edf4fb4c285bce
李齐一 <errgou@gmail.com>
2023-02-03 18:24:47 +0800

    add 部署脚本

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker start b21&&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build . -t node-blog-app &&
+docker kill app &&
+docker rm app &&
+docker run --name app --network=host -p 3000:3000 -d node-blog-app&&
+echo 'deploy success'
